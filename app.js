c6466ec40f1833874da38997ef291704194fe215
Rafael <rafael_ishikava@outlook.com>
2024-06-05 21:47:25 -0300

    Oh shit, local machine exposed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.sendFile("/home/rafael/hello_world/home.html")
+    res.sendFile(__dirname + "/home.html")
 })
 
 app.listen(port, () => {
