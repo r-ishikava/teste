b2969c1b1d40821f794b79e40acfadbb9646d734
Rafael <rafael_ishikava@outlook.com>
2024-06-05 21:41:17 -0300

    Ednaldo Agiota

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello, World")
+    res.sendFile("/home/rafael/hello_world/home.html")
 })
 
 app.listen(port, () => {
